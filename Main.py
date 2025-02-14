3c5e1994aa96dbc7e341c822dc9ae7c7139a66d1
FreakingCloel <mynameiscloel@gmail.com>
2025-02-13 20:05:27 -0600

    too much, but we should have recording as a module, a widget launch (quick launch with latch recording on), and a main file that just launches a screen with a button to record.

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+import pyaudio
 import speech_recognition as sr
 
 class VoiceNoteApp(App):
